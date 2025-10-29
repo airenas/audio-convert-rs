125dfa158d383213b94f01b150b24092402b46db
Airenas Vaiciunas <airenass@gmail.com>
2025-10-29 09:47:59 +0200

    fix: load tests

--- example/load/load.js
+++ example/load/load.js
@@ -1,10 +1,11 @@
 import { check, sleep } from 'k6';
-import grpc from 'k6/net/grpc';
 import encoding from 'k6/encoding';
+import grpc from 'k6/net/grpc';
 
 const conf = {
     // baseURL: __ENV.BASE_URL || "grpcbin.test.k6.io:9001",
-    url: 'host.docker.internal:50051'
+    url: 'host.docker.internal:50051',
+    format: __ENV.FORMAT || 'M4A',
 }
 
 
@@ -17,11 +18,28 @@ export let options = {
 const binaryData = open('./1.wav', 'b');
 const base64Data = encoding.b64encode(binaryData);
 console.log('🧪 Data length (base64):', base64Data.length);
+console.log('🧪 Test format:', conf.format);
 
 
 
 const client = new grpc.Client();
 client.load(['definitions'], 'audio_convert.proto');
+
+function to_enum_format(formatStr) {
+    switch (formatStr.toUpperCase()) {
+        case 'MP3':
+            return 1;
+        case 'M4A':
+            return 2;
+            case 'ULAW':
+            return 3;
+        default:
+            console.error('Unknown format:', formatStr);
+            return 0;
+    }
+}
+
+const formatEnum = to_enum_format(conf.format);
 
 export default () => {
     console.log('connecting: ' + conf.url);
@@ -29,9 +47,10 @@ export default () => {
         plaintext: true
     });
 
-    const data = { format: 2, 
+    const data = {
+        format: formatEnum,
         metadata: ['olia=aaa'],
-        data: base64Data, 
+        data: base64Data,
     };
     const response = client.invoke('/audio_convert.v1.AudioConverter/Convert', data);
 
